f1f5d1e72e8dee75134a23a0e519d3821941aa07
kakagoosse856 <alkaceriptv@gmail.com>
2026-01-29 13:12:49 +0100

    Update bein.py

--- bein.py
+++ bein.py
@@ -14,8 +14,8 @@ CHANNEL_NAMES = [
 # مصادر M3U
 # =====================
 M3U_SOURCES = [
-    "https://raw.githubusercontent.com/Walid533112/airmax/refs/heads/main/airmax.m3u"
-   "https://raw.githubusercontent.com/Yusufdkci/iptv/71fabe363ebf0c3d46ae0ce69f8e3202164b7edc/liste.m3u"
+    "https://raw.githubusercontent.com/Walid533112/airmax/refs/heads/main/airmax.m3u",
+    "https://raw.githubusercontent.com/Yusufdkci/iptv/71fabe363ebf0c3d46ae0ce69f8e3202164b7edc/liste.m3u"
 ]
 
 # =====================
